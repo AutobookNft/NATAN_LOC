06cdf8076b224d000bf7d1ac2f2a1d6333c9a2f6
fabio cherici <fabiocherici@gmail.com>
2025-11-06 10:00:00 +0100

    [FEAT] Sidebar con chat history condizionale
    - Chat history visibile solo in contesto natan/chat
    - Menu items evidenziano pagina corrente

--- laravel_backend/resources/views/components/natan/sidebar.blade.php
+++ laravel_backend/resources/views/components/natan/sidebar.blade.php
@@ -5,43 +5,45 @@
 
 <aside id="sidebar-desktop" class="hidden lg:w-70 lg:fixed lg:inset-y-0 lg:top-16 lg:z-40 lg:flex lg:flex-col">
     <div class="flex flex-col flex-1 overflow-y-auto w-70 bg-natan-blue">
-        {{-- Cronologia Chat --}}
-        <div class="p-4">
-            <h3 class="mb-3 text-xs font-bold tracking-wider uppercase font-institutional text-natan-blue-extra-light">
-                {{ __('natan.history.title') }}
-            </h3>
+        {{-- Cronologia Chat - Solo nel contesto chat --}}
+        @if(request()->routeIs('natan.chat'))
+            <div class="p-4">
+                <h3 class="mb-3 text-xs font-bold tracking-wider uppercase font-institutional text-natan-blue-extra-light">
+                    {{ __('natan.history.title') }}
+                </h3>
 
-            <div class="space-y-2">
-                {{-- Ultime 3 chat: sempre espanse --}}
-                @forelse(array_slice($chatHistory, 0, 3) as $chat)
-                    <x-natan.chat-history-item :chat="$chat" :expanded="true" />
-                @empty
-                    <p class="px-2 text-xs text-natan-blue-extra-light/70">
-                        {{ __('natan.history.empty') }}
-                    </p>
-                @endforelse
+                <div class="space-y-2">
+                    {{-- Ultime 3 chat: sempre espanse --}}
+                    @forelse(array_slice($chatHistory, 0, 3) as $chat)
+                        <x-natan.chat-history-item :chat="$chat" :expanded="true" />
+                    @empty
+                        <p class="px-2 text-xs text-natan-blue-extra-light/70">
+                            {{ __('natan.history.empty') }}
+                        </p>
+                    @endforelse
 
-                {{-- Chat precedenti: collassabili --}}
-                @if (count($chatHistory) > 3)
-                    <details class="group">
-                        <summary
-                            class="flex items-center justify-between px-2 py-2 text-xs rounded-lg cursor-pointer text-natan-blue-extra-light hover:bg-white/10">
-                            <span>{{ __('natan.history.previous', ['count' => count($chatHistory) - 3]) }}</span>
-                            <x-natan.icon name="chevron-down"
-                                class="w-4 h-4 transition-transform group-open:rotate-180" />
-                        </summary>
-                        <div class="pl-2 mt-2 space-y-1 border-l border-white/10">
-                            @foreach (array_slice($chatHistory, 3) as $chat)
-                                <x-natan.chat-history-item :chat="$chat" :expanded="false" />
-                            @endforeach
-                        </div>
-                    </details>
-                @endif
+                    {{-- Chat precedenti: collassabili --}}
+                    @if (count($chatHistory) > 3)
+                        <details class="group">
+                            <summary
+                                class="flex items-center justify-between px-2 py-2 text-xs rounded-lg cursor-pointer text-natan-blue-extra-light hover:bg-white/10">
+                                <span>{{ __('natan.history.previous', ['count' => count($chatHistory) - 3]) }}</span>
+                                <x-natan.icon name="chevron-down"
+                                    class="w-4 h-4 transition-transform group-open:rotate-180" />
+                            </summary>
+                            <div class="pl-2 mt-2 space-y-1 border-l border-white/10">
+                                @foreach (array_slice($chatHistory, 3) as $chat)
+                                    <x-natan.chat-history-item :chat="$chat" :expanded="false" />
+                                @endforeach
+                            </div>
+                        </details>
+                    @endif
+                </div>
             </div>
-        </div>
 
-        {{-- Separatore --}}
-        <hr class="my-2 border-white/10" />
+            {{-- Separatore --}}
+            <hr class="my-2 border-white/10" />
+        @endif
 
         {{-- Feature Menu (Bottom Section) --}}
         <div class="flex-1 px-4 pb-4">
